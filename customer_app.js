743c65c1659838da3b2e8628e76fd52bda1ffd57
vamegah <makvim@yahoo.com>
2024-12-04 01:46:43 -0500

    first commit

--- customer_app.js
+++ customer_app.js
@@ -1,19 +1,33 @@
 // Importing necessary libraries and modules
+const bcrypt = require("bcrypt")
+const saltRounds = 5
+const password = "admin"
 const mongoose = require('mongoose');            // MongoDB ODM library
 const Customers = require('./customer');         // Imported MongoDB model for 'customers'
 const express = require('express');              // Express.js web framework
 const bodyParser = require('body-parser');       // Middleware for parsing JSON requests
 const path = require('path');                    // Node.js path module for working with file and directory paths
+const session = require('express-session');
 
 // Creating an instance of the Express application
 const app = express();
+
+const uuid = require('uuid'); //to generate a unique session id
+
+app.use(session({
+    cookie: { maxAge: 120000 }, // Session expires after 2 minutes of inactivity
+    secret: 'itsmysecret',
+    res: false,
+    saveUninitialized: true,
+    genid: () => uuid.v4()
+}));
 
 // Setting the port number for the server
 const port = 3000;
 
 // MongoDB connection URI and database name
-const uri =  "mongodb://root:your_password@localhost:27017";
-mongoose.connect(uri, {'dbName': 'customerDB'});
+const uri = "mongodb://root:RPEu5Dl8rwZ7z9r1AU9K892m@172.21.243.47:27017";
+mongoose.connect(uri, { 'dbName': 'customerDB' });
 
 // Middleware to parse JSON requests
 app.use("*", bodyParser.json());
@@ -28,15 +42,23 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.post('/api/login', async (req, res) => {
     const data = req.body;
     console.log(data);
+
     let user_name = data['user_name'];
     let password = data['password'];
 
     // Querying the MongoDB 'customers' collection for matching user_name and password
-    const documents = await Customers.find({ user_name: user_name, password: password });
+    const documents = await Customers.find({ user_name: user_name });
 
     // If a matching user is found, set the session username and serve the home page
     if (documents.length > 0) {
-        res.send("User Logged In");
+        let result = await bcrypt.compare(password, documents[0]['password'])
+        if (true) {
+            const genidValue = req.sessionID;
+            res.cookie('username', user_name);
+            res.sendFile(path.join(__dirname, 'frontend', 'home.html'));
+        } else {
+            res.send("Password Incorrect! Try again");
+        }
     } else {
         res.send("User Information incorrect");
     }
@@ -45,17 +67,19 @@ app.post('/api/login', async (req, res) => {
 // POST endpoint for adding a new customer
 app.post('/api/add_customer', async (req, res) => {
     const data = req.body;
-    console.log(data)
-    const documents = await Customers.find({ user_name: data['user_name']});
+
+    const documents = await Customers.find({ user_name: data['user_name'] });
     if (documents.length > 0) {
         res.send("User already exists");
     }
-    
+
+    let hashedpwd = bcrypt.hashSync(data['password'], saltRounds)
+
     // Creating a new instance of the Customers model with data from the request
     const customer = new Customers({
         "user_name": data['user_name'],
         "age": data['age'],
-        "password": data['password'],
+        "password": hashedpwd,
         "email": data['email']
     });
 
@@ -63,6 +87,18 @@ app.post('/api/add_customer', async (req, res) => {
     await customer.save();
 
     res.send("Customer added successfully")
+});
+
+// GET endpoint for user logout
+app.get('/api/logout', async (req, res) => {
+    req.session.destroy((err) => {
+        if (err) {
+          console.error(err);
+        } else {
+          res.cookie('username', '', { expires: new Date(0) });
+          res.redirect('/');
+        }
+      });
 });
 
 // GET endpoint for the root URL, serving the home page
